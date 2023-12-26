d47dc54e300a6c5cda42b19090a8538a54ca2d4c
ncmk2k2 <ncmkhuong1010@gmail.com>
2023-12-26 10:10:11 +0700

    update DangKyThiLai

--- src/Apis/MotCua/KhaoThi/apiDangKyThiLai.js
+++ src/Apis/MotCua/KhaoThi/apiDangKyThiLai.js
@@ -1,5 +1,11 @@
 import http from "../../../Configs/http";
 
+// lấy đợt thi
+export const getTenDotDKThiLai = () => {
+	return http.get("/SP_MC_KT_DangKyThi_TiepNhan/EDU_Load_TenDot");
+};
+
+// lấy danh sách học phần có thể đăng ký
 export const getAllHocPhanDKThiLai = (MaSinhVien = "", tenDot = "", loaiThi = "") => {
 	return http.get(`SP_MC_KT_DangKyThi_TiepNhan/EDU_Load_R_Para_MaSinhVien_DangKyThi`, {
 		params: {
@@ -10,6 +16,20 @@ export const getAllHocPhanDKThiLai = (MaSinhVien = "", tenDot = "", loaiThi = ""
 	});
 };
 
+// kiểm tra trùng
+export const getKiemTraTrung = (MaSinhVien = "", TenDot="", TenMonHoc="", MaMonHoc="", YeuCau) => {
+	return http.get(`SP_MC_KT_DangKyThi_TiepNhan/KiemTraTrung`, {
+		params: {
+			MC_KT_DangKyThi_MaSinhVien: MaSinhVien,
+			MC_KT_DangKyThi_TenDot: TenDot,
+			MC_KT_DangKyThi_TenMonHoc: TenMonHoc,
+			MC_KT_DangKyThi_MaMonHoc: MaMonHoc,
+			MC_KT_DangKyThi_YeuCau: YeuCau
+		},
+	});
+}
+
+// gửi yêu cầu
 export const postDangKyThiLai = (data = {}) => {
 	return http.post("SP_MC_KT_DangKyThi_TiepNhan/Add_Para", data);
 };
--- src/Pages/Clients/MotCua/KhaoThi/DangKyThiLai/FormDangKyThiLai.jsx
+++ src/Pages/Clients/MotCua/KhaoThi/DangKyThiLai/FormDangKyThiLai.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import DanhSachLichThi from "./DanhSachLichThi";
+import DanhSachHocPhan from "./DanhSachHocPhan";
 
-function FormDangKyThiLai(){
-
-    const [lichThi, setLichThi] = useState([])
-    const [hocKy, setHocKy] = useState('')
-    const [lyDo, setLyDo] = useState('')
+function FormDangKyThiLai(props){
+    const {listHocPhan, hocKy, setHocKy, lyDo, setLyDo, listHocKy, lyDoKhac, setLyDoKhac, handleRowSelection, handleSubmitData} = props;
 
     return (
         <div className="border-[#336699] border border-solid mt-5 rounded-md">
@@ -15,22 +12,37 @@ function FormDangKyThiLai(){
                     <span className="block pr-10">Học kỳ (*)</span>
                     <select className="flex-1 max-w-[75%] p-2 rounded-md border border-solid border-gray-300" value={hocKy} onChange={e => setHocKy(e.target.value)}>
                         <option value="">Chọn học kỳ</option>
-                        <option>test</option>
+                        {listHocKy.map((e, index) => (
+                            <option key={index} value={e.TenDot}>{e.TenDot}</option>
+                        ))}
                     </select>
                 </div>
                 <div className="w-[75%] flex justify-between items-center">
                     <span className="block pr-10">Loại thi</span>
                     <select disabled className="flex-1 max-w-[75%] p-2 rounded-md border border-solid border-gray-300">
-                        <option>Thi Lại</option>
+                        <option value="Thi Lại">Thi Lại</option>
                     </select>
                 </div>
-                <div className="w-[75%] flex justify-between items-center" value={lyDo} onChange={e => setLyDo(e.target.value)}>
+                <div className="w-[75%] flex justify-between items-center">
                     <span className="block pr-10">Lý do (*)</span>
-                    <select className="flex-1 max-w-[75%] p-2 rounded-md border border-solid border-gray-300">
+                    <select className="flex-1 max-w-[75%] p-2 rounded-md border border-solid border-gray-300" value={lyDo} onChange={e => {setLyDo(e.target.value); setLyDoKhac('')}}>
                         <option value="">Chọn lý do</option>
-                        <option value="test">test</option>
+                        <option value='0'>Trùng lịch thi</option>
+                        <option value='1'>Lỗi Website</option>
+                        <option value='2'>Khác hệ, loại hình đào tạo</option>
+                        <option value='3'>Thi không theo kế hoạch</option>
+                        <option value='4'>Lý do khác</option>
                     </select>
                 </div>
+                {
+                    lyDo === '4' && (
+                    <div className="w-[75%] flex justify-between items-start">
+                        <span className="block pr-10">Chi tiết lý do (*)</span>
+                        <textarea value={lyDoKhac} onChange={e => setLyDoKhac(e.target.value)} className="flex-1 max-w-[75%] p-2 rounded-md border border-solid border-gray-300">
+                        </textarea>
+                    </div>
+                    )
+                }
             </form>
 
             {(hocKy === '' && lyDo != '') || (hocKy != '' && lyDo === '') ? (
@@ -42,12 +54,16 @@ function FormDangKyThiLai(){
 
             {(hocKy != '' && lyDo != '') && (
             <div className="flex flex-col justify-center items-center pb-4">
-                <DanhSachLichThi lichThi={[]}/>
-                <button className="mt-8 px-5 py-3 border-2 border-solid text-[#245D7C] border-[#245D7C] rounded-md font-bold transition-all duration-200 hover:bg-[#245D7C] hover:text-white">Gửi Yêu Cầu</button>
+                <DanhSachHocPhan listHocPhan={listHocPhan} handleRowSelection={handleRowSelection}/>
+                <button onClick={handleSubmitData} className="mt-8 px-5 py-3 border-2 border-solid text-[#245D7C] border-[#245D7C] rounded-md font-bold transition-all duration-200 hover:bg-[#245D7C] hover:text-white">Gửi Yêu Cầu</button>
             </div >
             )}
         </div>
     )
+}
+
+FormDangKyThiLai.propTypes = {
+
 }
 
 export default FormDangKyThiLai;
