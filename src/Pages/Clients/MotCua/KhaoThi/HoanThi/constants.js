62c8ad505b4e6e56da18785a4280a457f710e635
Think <73821256+tranthinh-coding@users.noreply.github.com>
2023-12-27 17:16:48 +0700

    KhaoThi_HoanThi: add more constants

--- src/Pages/Clients/MotCua/KhaoThi/HoanThi/constants.js
+++ src/Pages/Clients/MotCua/KhaoThi/HoanThi/constants.js
@@ -1,35 +1,40 @@
 export const home = {
   path: '/motcua',
-  title: 'Bộ phận một cửa',
+  title: 'Bộ phận một cửa'
 }
 
 export const breadcrumbs = [
   {
     path: '/motcua/khaothi',
-    title: 'Khảo thí',
+    title: 'Khảo thí'
   },
   {
     path: '/motcua/khaothi/hoanthi',
-    title: 'Hoãn thi',
-  },
+    title: 'Hoãn thi'
+  }
 ]
 
-export const LY_DO_KHAC = 2
+export const LY_DO_KHAC = 3
 
 export const listLyDo = [
   {
+    id: 0,
+    title: 'Trùng lịch thi',
+    value: 0
+  },
+  {
     id: 1,
     title: 'Đi viện hoặc theo y/c bác sĩ',
-    value: 0,
+    value: 1
   },
   {
     id: 2,
     title: 'Thực hiện nhiệm vụ nhà trường giao',
-    value: 1,
+    value: 2
   },
   {
     id: 3,
     title: 'Lý do khác',
-    value: LY_DO_KHAC,
-  },
+    value: LY_DO_KHAC
+  }
 ]
