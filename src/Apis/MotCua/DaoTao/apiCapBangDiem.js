760dbd88c2ce7e91f800a4beafb3fcc4639c66b8
Think <73821256+tranthinh-coding@users.noreply.github.com>
2024-01-03 00:23:22 +0700

    MotCua_DaoTao_XacNhan: updated params

--- src/Apis/MotCua/DaoTao/apiCapBangDiem.js
+++ src/Apis/MotCua/DaoTao/apiCapBangDiem.js
@@ -3,8 +3,8 @@ import http from '@/Configs/http'
 export const capBangDiemKiemTraTrung = ({ maSinhVien, yeuCau, loaiBangDiem }) =>
   http.get('SP_MC_DT_CapBangDiem_TiepNhan/BangDiem_TiepNhan_KiemTraTrung', {
     params: {
-      MC_DT_BangDiem_MaSinhVien: maSinhVien,
-      MC_DT_BangDiem_YeuCau: yeuCau,
+      MC_DT_CapBangDiem_MaSinhVien: maSinhVien,
+      MC_DT_CapBangDiem_YeuCau: yeuCau,
       MC_DT_CapBangDiem_YeuCau_LoaiBangDiem: loaiBangDiem
     }
   })
