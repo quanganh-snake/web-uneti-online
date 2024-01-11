f62eb40b6b7856c65fe6e27671055a87aca45724
Think <tranthinh.own@gmail.com>
2024-01-11 19:06:08 +0700

    feat: add components Accordion

--- src/Pages/Clients/HocTap/KetQuaHocTap/KetQuaHocTap.jsx
+++ src/Pages/Clients/HocTap/KetQuaHocTap/KetQuaHocTap.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import Accordion from '@/Components/Base/Accordion/Accordion'
 
 export default function KetQuaHocTap() {
   return (
-    <div>KetQuaHocTap</div>
+    <>
+      <Accordion>
+        <Accordion.Label className="bg-red-400">
+          Học kỳ 1 (2021 - 2022)
+        </Accordion.Label>
+        <Accordion.Content className="bg-red-300 mt-1">Content</Accordion.Content>
+      </Accordion>
+
+      <Accordion>
+        <Accordion.Label className="bg-red-400">
+          <div>Học kỳ 1 (2021 - 2022)</div>
+        </Accordion.Label>
+
+        <Accordion.Content className="bg-red-300 mt-2">Content</Accordion.Content>
+      </Accordion>
+    </>
   )
 }
