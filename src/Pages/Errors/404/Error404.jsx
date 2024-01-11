d8b2de8f9e135cc5bed15da82460901ba63e1098
Think <73821256+tranthinh-coding@users.noreply.github.com>
2024-01-11 20:32:00 +0700

    feat: update error page: 404

--- src/Pages/Errors/404/Error404.jsx
+++ src/Pages/Errors/404/Error404.jsx
@@ -4,45 +4,36 @@ import logoUNETI from '../../../assets/Images/LOGO_UNETI.ico'
 
 function Error404() {
   return (
-    <div className="relative right-0 left-0 w-full h-screen bg-sky-300 rounded-lg shadow-md">
-      <div className="px-20 h-full flex items-center justify-center">
-        <div className="grid grid-cols-12 flex-row justify-center items-center gap-16">
-          <div className="col-span-8">
-            <div className="flex flex-col justify-center items-center">
-              <div className="uneti-logo flex justify-center mb-5">
-                <img src={logoUNETI} alt="" />
-              </div>
-              <h2 className="text-5xl text-white font-bold mb-4">
-                UNETI - Page Not Found...
-              </h2>
-              <p className="text-white">Không tìm thấy trang mà bạn cần tìm!</p>
-              <div className="my-6">
-                <Link
-                  to="/uneti"
-                  className="px-3 py-2 text-sky-800 bg-slate-100 border border-slate-200 rounded-full hover:opacity-70"
-                >
-                  Quay về trang chủ &rarr;
-                </Link>
-              </div>
+    <div className="max-w-7xl mx-auto text-slate-700">
+      <div className="px-20 flex items-center justify-evenly w-full h-96">
+        <div className="h-full relative">
+          <div className="flex flex-col">
+            <div className="flex items-center gap-5 relative top-0 left-0">
+              <span className="text-[140px] font-bold text-slate-300">4</span>
+              <span className="text-[140px] font-bold text-slate-300">0</span>
+              <span className="text-[140px] font-bold text-slate-300">4</span>
+              <h3 className="text-[90px] font-bold text-slate-600 absolute bottom-0 right-0">
+                Oops!
+              </h3>
             </div>
           </div>
-          <div className="col-span-4">
-            <div className="w-full h-full ">
-              <div className="w-[300px] h-[300px] rounded-full bg-white flex justify-center items-center shadow-lg">
-                <p className="text-9xl font-bold">
-                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 ">
-                    4
-                  </span>
-                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 ">
-                    0
-                  </span>
-                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-500 ">
-                    4
-                  </span>
-                </p>
+          <div className="mt-4">
+            <div>
+              <h3 className="text-2xl">Uneti - Page not found</h3>
+              <div className="max-w-sm mt-1">
+                Trang bạn đang truy cập không tồn tại, vui lòng kiểm tra lại
+                đường dẫn!
               </div>
             </div>
+            <Link to="/uneti">
+              <button className="mt-8 border-none outline-none rounded-xl bg-[#336699] text-white px-8 py-2">
+                Về trang chủ
+              </button>
+            </Link>
           </div>
+        </div>
+        <div className="uneti-logo flex justify-center mb-5">
+          <img src={logoUNETI} alt="" />
         </div>
       </div>
     </div>
