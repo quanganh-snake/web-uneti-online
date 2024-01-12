92b556d35181389c51bbcccf25bc54280d9efd54
Think <73821256+tranthinh-coding@users.noreply.github.com>
2024-01-12 15:56:52 +0700

    fix(components): [Accordion]  allow classname, change content height when window resize

--- src/Components/Base/AccordionContent/AccordionContent.jsx
+++ src/Components/Base/AccordionContent/AccordionContent.jsx
@@ -5,6 +5,9 @@ import { AccordionContext } from '../Accordion/constants'
 
 import './AccordionContent.scss'
 import { transformCls } from '@/Services/Utils/reactUtils'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useCallback } from 'react'
 
 export default function AccordionContent({
   children,
@@ -17,6 +20,22 @@ export default function AccordionContent({
 
   const contentRef = useRef()
 
+  const [height, setHeight] = useState('0')
+
+  const calcContentHeight = useCallback(() => {
+    setHeight(accordionCtx.isOpen ? contentRef.current.scrollHeight : '0')
+  }, [accordionCtx.isOpen])
+
+  useEffect(() => {
+    calcContentHeight()
+
+    window.addEventListener('resize', calcContentHeight)
+
+    return () => {
+      window.removeEventListener('resize', calcContentHeight)
+    }
+  }, [accordionCtx.isOpen, calcContentHeight])
+
   if (!accordionCtx?.isAccordion) {
     console.warn(
       '~ Components - Accordion - Content: AccordionContent need to called inside the Accordion component',
@@ -28,7 +47,7 @@ export default function AccordionContent({
     <div
       className={transformCls([className, bem.e('wrapper')])}
       style={{
-        height: accordionCtx.isOpen ? contentRef.current.clientHeight : '0',
+        height: height,
       }}
       {...attrs}
     >
