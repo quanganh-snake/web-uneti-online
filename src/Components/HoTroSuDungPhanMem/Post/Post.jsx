4b354a81a49db0a91cec5aca8f92ab9e38de0a9c
Think <73821256+tranthinh-coding@users.noreply.github.com>
2024-01-12 15:38:51 +0700

    fix: HoTroSuDungPhanMem - handle download file, change style

--- src/Components/HoTroSuDungPhanMem/Post/Post.jsx
+++ src/Components/HoTroSuDungPhanMem/Post/Post.jsx
@@ -2,36 +2,61 @@ import { useBem } from '@/Services/Hooks'
 import PropTypes from 'prop-types'
 
 import './Post.scss'
+import { handlePreviewFileBase64 } from '@/Services/Utils/fileUtils'
+import { convertBufferToBase64 } from '@/Services/Utils/stringUtils'
 
-const LINK_YOUTUBE_REGEX =
-  /https:\/\/www\.youtube\.com\/playlist\?list=([^\s]+)/
+const LINK_YOUTUBE_REGEX = /https:\/\/www\.youtube\.com\/\S+/g
 
-export const Post = ({ DT_CVNB_TBGD_TL_Ten, DT_CVNB_TBGD_TL_GhiChu }) => {
+export const Post = ({
+  DT_CVNB_TBGD_TL_Ten,
+  DT_CVNB_TBGD_TL_GhiChu,
+  DT_CVNB_TBGD_TL_TenFile,
+  DT_CVNB_TBGD_TL_DataFile,
+}) => {
   const bem = useBem('post')
 
-  let video = ''
+  let video = DT_CVNB_TBGD_TL_GhiChu?.match(LINK_YOUTUBE_REGEX)?.[0]
 
-  const linkYoutubeInDescription =
-    DT_CVNB_TBGD_TL_GhiChu?.match(LINK_YOUTUBE_REGEX)
-  if (linkYoutubeInDescription) {
-    video = linkYoutubeInDescription[0]
+  const ghiChu = DT_CVNB_TBGD_TL_GhiChu.replace(LINK_YOUTUBE_REGEX, '')
+
+  const userCanDownloadFile =
+    DT_CVNB_TBGD_TL_TenFile && DT_CVNB_TBGD_TL_DataFile
+
+  const handleDownloadFile = () => {
+    handlePreviewFileBase64(
+      DT_CVNB_TBGD_TL_TenFile,
+      convertBufferToBase64(DT_CVNB_TBGD_TL_DataFile?.data),
+    )
   }
 
-  const handleOpenVideo = () => {
-    if (!video) return
-    window.open(video)
-  }
   return (
     <div className={bem.b()}>
       <h3 className={bem.e('title')}>{DT_CVNB_TBGD_TL_Ten}</h3>
 
-      <p className={bem.e('description')}>{DT_CVNB_TBGD_TL_GhiChu}</p>
+      <p className={`${bem.e('description')} mb-4`}>{ghiChu}</p>
 
-      {video && (
-        <button className={bem.e('button')} onClick={handleOpenVideo}>
-          Xem video
-        </button>
-      )}
+      <div className="flex justify-between items-center">
+        {video && (
+          <a
+            href={video}
+            target="_blank"
+            className={`${bem.e(
+              'link',
+            )} border-b-2 text-uneti-primary cursor-pointer border-b-uneti-primary`}
+            rel="noreferrer"
+          >
+            Xem video
+          </a>
+        )}
+        {userCanDownloadFile && (
+          <div
+            className={bem.e('download-button')}
+            onClick={handleDownloadFile}
+          >
+            Tải file
+          </div>
+        )}
+      </div>
     </div>
   )
 }
