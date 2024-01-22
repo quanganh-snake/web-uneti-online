75d3e4749fc310174bcd157759ef61e12719fd86
ncmk2k2 <ncmkhuong1010@gmail.com>
2024-01-22 16:46:36 +0700

    update menu Ontap

--- src/Pages/Clients/HocTap/OnLuyen/OnTap/DanhSachCauHoi/DanhSachCauHoi.jsx
+++ src/Pages/Clients/HocTap/OnLuyen/OnTap/DanhSachCauHoi/DanhSachCauHoi.jsx
@@ -10,14 +10,13 @@ import {
 } from '@/Apis/HocTap/apiOnLuyenThiThu'
 import CauHoi from '@/Components/HocTap/OnTap/CauHoi'
 import { getAllMonHoc } from '@/Apis/HocTap/apiHocTap'
-import MenuIcon from './MenuIcon'
-import CloseIcon from './CloseIcon'
+import { useBem } from '@/Services/Hooks'
+import './DanhSachCauHoi.scss'
 
 function DanhSachDeThi() {
   const uLocation = useLocation()
   const navigate = useNavigate()
-
-  const [showMenu, setShowMenu] = useState(false)
+  const bem = useBem('DanhSachDeThi')
 
   const maMonHoc = uLocation.pathname.split('/').at(-1).toString()
 
@@ -71,9 +70,11 @@ function DanhSachDeThi() {
     })
   }, [maHocPhan])
 
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <>
-      <div className="flex flex-col justify-start items-center gap-4 bg-white rounded-[26px] mb-4 p-4">
+    <div>
+      <div className="flex flex-col text-center justify-start items-center gap-4 bg-white rounded-[26px] mb-4 p-4">
         <h3 className="text-uneti-primary font-bold text-4xl">
           {monHoc.TenMonHoc}
         </h3>
@@ -82,7 +83,7 @@ function DanhSachDeThi() {
         </span>
       </div>
       <div className="flex justify-center items-start gap-4">
-        <div className="w-full">
+        <div className="w-full border-b-2 border-solid border-b-white">
           <div className="flex flex-col gap-3 bg-white rounded-[26px] p-4">
             {listCauHoi.map((e, index) => (
               <CauHoi key={index} STT={index + 1} {...e} />
@@ -90,16 +91,84 @@ function DanhSachDeThi() {
           </div>
         </div>
 
-        <div className="fixed right-4 top-48">
-          <MenuIcon />
-          <CloseIcon />
-        </div>
-
-        <div className="md:w-[30%] rounded-[26px] bg-white p-4 fixed right-4 top-50">
-          ddknj
+        <div
+          className={`fixed top-[240px] right-[20px] transition-all duration-200 bg-white rounded-xl overflow-hidden shadow-md flex flex-col md:w-[30%] md:static`}
+        >
+          <div className="w-full bg-uneti-primary p-2 text-white flex justify-between items-center">
+            <span className="text-white flex justify-center items-center gap-2 text-center">
+              <svg
+                width="24"
+                height="25"
+                viewBox="0 0 24 25"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12 8V13"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M9 2H15"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M4.75 9.5C3.65 10.96 3 12.78 3 14.75C3 19.58 6.92 23.5 11.75 23.5C16.58 23.5 20.5 19.58 20.5 14.75C20.5 9.92 16.58 6 11.75 6C10.49 6 9.3 6.26 8.22 6.74"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <span>90:00</span>
+            </span>
+            <button
+              onClick={() => {
+                setShowMenu((_showMenu) => !_showMenu)
+              }}
+              className={`duration-200 ml-10 md:ml-0 ${
+                showMenu ? '-rotate-90' : 'rotate-0'
+              }`}
+            >
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M11.1897 7.93994L8.56969 10.5599C7.79969 11.3299 7.79969 12.5899 8.56969 13.3599L15.0897 19.8799"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M15.0898 4.04004L14.0498 5.08004"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className={`transition-all duration-200`}>show</div>
+          <div className="w-full bg-uneti-primary p-2 text-white flex justify-center items-center border-t-2 border-solid border-t-white">
+            <button>Nộp Bài</button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
